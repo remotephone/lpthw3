e18351f52a37ffc69c2e2ac694c3b7cd27acecf5
remotephone@gmail.com <remotephone@gmail.com>
2018-01-15 03:57:35 +0000

    finished 3

--- ex1_10/ex3.py
+++ ex1_10/ex3.py
@@ -10,3 +10,14 @@ print(3 + 2 + 1 -5 +4 % 2 - 1 /4 + 6)
 print("Is it true that 3 + 2 < 5 - 7?")
 
 print(3 + 2 < 5 -7)
+
+print("What is 3 + 2?". 3 + 2)
+print("What is 5 - 7?", 5 -7)
+
+print ("Oh that is why it is False.")
+
+print("How aobut some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
