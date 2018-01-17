a256196e8c01db9bc258016631af7e8e40e99ca3
remotephone <remotephone@gmail.com>
2018-01-16 23:27:51 -0600

    did a few

---
+++ ex11_20/ex13_1.py
@@ -0,0 +1,6 @@
+# import the modules
+from sys import argv
+script, second = argv
+
+print("The name of a new script is: ", script)
+print("It takes a second argument: ", second)
---
+++ ex11_20/ex13_2.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, arg1, arg2, arg3, arg4 = argv
+
+print("This is my script name: ", script)
+print("Here is part 2: ", arg1)
+print("Here is part 3: ", arg2)
+print("Here is part 4: ", arg3)
+print("Here is part 5: ", arg4)
