da5a863e21cb9595f5b11532623339d5fab4a414
remotephone <remotephone@gmail.com>
2018-02-01 22:11:25 -0600

    additional 14

---
+++ ex11_20/ex14-1.py
@@ -0,0 +1,26 @@
+import sys
+from sys import argv
+
+script, user_name, type = argv
+prompt = '> '
+
+print(f"Hi {user_name}, I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print(f"What {type} food do you like?")
+food = input(prompt)
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure what that is.
+And you have a {computer} computer. Nice.
+Your favorite {type} food is {food}.
+""")
