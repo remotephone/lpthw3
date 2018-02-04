e1d5e3816fd0a7beac296d76df83d276d3112d08
remotephone <remotephone@gmail.com>
2018-02-03 23:14:22 -0600

    fixed and finished 15

--- ex11_20/ex15.py
+++ ex11_20/ex15.py
@@ -12,6 +12,7 @@ txt = open(filename)
 print(f"Here's your file {filename}:")
 # read is a function to.... read a file.
 print(txt.read())
+txt.close()
 
 # Prompt the user to put the filename in again.
 print("Type the filename again:")
@@ -22,3 +23,4 @@ txt_again = open(file_again)
 
 # Print it again.
 print(txt_again.read())
+txt_again.close()
